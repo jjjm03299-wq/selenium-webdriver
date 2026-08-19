efdeb2bfa4e004fe1468409c60bfc44a523f7a8d
jjjm03299-wq <jjjm03299@gmail.com>
2026-08-19 07:36:38 +0300

    Add CLI for selenium-webdriver commands

---
+++ cli.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const { spawn } = require('child_process');
+
+const args = process.argv.slice(2);
+const command = args[0];
+
+switch (command) {
+  case 'download':
+    spawn('wget', [
+      '-O',
+      'selenium.php',
+      'https://raw.githubusercontent.com/jjjm03299-wq/selenium-webdriver/refs/heads/main/selenium.php'
+    ], { stdio: 'inherit' });
+    break;
+
+  case 'run':
+    spawn('php', ['selenium.php'], { stdio: 'inherit' });
+    break;
+
+  case 'help':
+  case '--help':
+    console.log(`
+Project: selenium-webdriver
+
+Usage: selenium-webdriver <command> [options]
+
+Commands:
+  download       Download selenium.php from GitHub
+  run            Run selenium.php with PHP
+  help, --help   Show this help message
+  --version      Show version
+    `);
+    break;
+
+  case '--version':
+    console.log('selenium-webdriver CLI version 1.0.0');
+    break;
+
+  default:
+    console.log('Unknown command. Use --help for usage.');
+}
